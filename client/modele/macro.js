3fe8d70eee88c21c4040dece530a9d595138bf25
Florian_Falkner <falknerflorian@hotmail.fr>
2020-03-19 10:30:14 +0100

    API Déplacement pions

--- client/modele/macro.js
+++ client/modele/macro.js
@@ -1,215 +1,56 @@
-var tabCases = {
-    "case0" : new THREE.Vector3(0.335*11.5, 2, 0.335*11.5) 
-, "case1"  : new THREE.Vector3(0.335*10, 2, 0.335*11.5) 
-, "case2" : new THREE.Vector3(0.335*9, 2, 0.335*11.5) 
-, "case3"  : new THREE.Vector3(0.335*8, 2, 0.335*11.5) 
-, "case4"  : new THREE.Vector3(0.335*7, 2, 0.335*11.5) 
-, "case5"  : new THREE.Vector3(0.335*6, 2, 0.335*11.5) 
-, "case6"  : new THREE.Vector3(0.335*5, 2, 0.335*11.5) 
-, "case7"  : new THREE.Vector3(0.335*4, 2, 0.335*11.5) 
-, "case8"  : new THREE.Vector3(0.335*3, 2, 0.335*11.5) 
-, "case9"  : new THREE.Vector3(0.335*2, 2, 0.335*11.5) 
-
-, "case10"  : new THREE.Vector3(0.335*1,2,0.335*11.5) 
-, "case11"  : new THREE.Vector3(0.335*1,2,0.335*10) 
-, "case12"  : new THREE.Vector3(0.335*1,2,0.335*9) 
-, "case13"  : new THREE.Vector3(0.335*1,2,0.335*8) 
-, "case14"  : new THREE.Vector3(0.335*1,2,0.335*7) 
-, "case15"  : new THREE.Vector3(0.335*1,2,0.335*6) 
-, "case16"  : new THREE.Vector3(0.335*1,2,0.335*5) 
-, "case17"  : new THREE.Vector3(0.335*1,2,0.335*4) 
-, "case18"  : new THREE.Vector3(0.335*1,2,0.335*3) 
-, "case19"  : new THREE.Vector3(0.335*1,2,0.335*2)
-
-, "case20"  : new THREE.Vector3(0.335*1,2,0.335*1) 
-, "case21"  : new THREE.Vector3(0.335*2,2,0.335*1) 
-, "case22"  : new THREE.Vector3(0.335*3,2,0.335*1) 
-, "case23"  : new THREE.Vector3(0.335*4,2,0.335*1) 
-, "case24"  : new THREE.Vector3(0.335*5,2,0.335*1) 
-, "case25"  : new THREE.Vector3(0.335*6,2,0.335*1) 
-, "case26"  : new THREE.Vector3(0.335*7,2,0.335*1) 
-, "case27"  : new THREE.Vector3(0.335*8,2,0.335*1) 
-, "case28"  : new THREE.Vector3(0.335*9,2,0.335*1) 
-, "case29"  : new THREE.Vector3(0.335*10,2,0.335*1) 
-
-, "case30"  : new THREE.Vector3(0.335*11.5,2,0.335*1) 
-, "case31"  : new THREE.Vector3(0.335*11.5,2,0.335*2) 
-, "case32"  : new THREE.Vector3(0.335*11.5,2,0.335*3) 
-, "case33"  : new THREE.Vector3(0.335*11.5,2,0.335*4) 
-, "case34"  : new THREE.Vector3(0.335*11.5,2,0.335*5) 
-, "case35"  : new THREE.Vector3(0.335*11.5,2,0.335*6) 
-, "case36"  : new THREE.Vector3(0.335*11.5,2,0.335*7) 
-, "case37"  : new THREE.Vector3(0.335*11.5,2,0.335*8) 
-, "case38"  : new THREE.Vector3(0.335*11.5,2,0.335*9) 
-, "case39"  : new THREE.Vector3(0.335*11.5,2,0.335*10) 
-
-} 
+var pawn = [
+        "moto", "citroen C4", 
+        "boat", "montgolfiere", 
+        "overboard", "tracteur", 
+        "bus", "camion"
+];
 
 
-function no(essai){
-	switch(essai){
-		case "case0":
-				return "case0";
-				break;
-
-		case "case1":
-				return "case1";
-				break;
-
-		case "case2":
-				return "case2";
-				break;
-
-		case "case3":
-				return "case3";
-				break;
-
-		case "case4":
-				return "case4";
-				break;
-
-		case "case5":
-                return "case5";
-                break;
-
-        case "case6":
-                return "case6";
-                break;
-                    
-        case "case7":
-                return "case7";
-                break;
-                        
-        case "case8":
-                return "case8";
-                break;
-                            
-        case "case9":
-                return "case9";
-                break;
-                                
-        case "case10":
-                return "case10";
-                break;
-                                    
-        case "case11":
-                return "case11";
-                break;
-                                        
-        case "case12":
-                return "case12";
-                break;
-                                            
-        case "case13":
-                return "case13";
-                break;
-                                                
-        case "case14":
-                return "case14";
-                break;
-                                                    
-        case "case15":
-                return "case15";
-                break;
-                                                        
-        case "case16":
-                return "case16";
-                break;
-                                                            
-        case "case17":
-                return "case17";
-                break;
-
-        case "case18":
-                return "case18";
-                break;
-
-        case "case19":
-                return "case19";
-                break;
-
-        case "case20":
-                return "case20";
-                break;
-
-        case "case21":
-                return "case21";
-                break;
-
-        case "case22":
-                return "case22";
-                break;
-
-        case "case23":
-                return "case23";
-                break;
-
-        case "case24":
-                return "case24";
-                break;
-
-        case "case25":
-                return "case25";
-                break;
-
-        case "case26":
-                return "case26";
-                break;
-
-        case "case27":
-                return "case27";
-                break;
-
-        case "case28":
-                return "case28";
-                break;
-
-        case "case29":
-                return "case29";
-                break;
-
-        case "case30":
-                return "case30";
-                break;
-
-        case "case31":
-                return "case31";
-                break;
-
-        case "case32":
-                return "case32";
-                break;
-
-        case "case33":
-                return "case33";
-                break;
-
-        case "case34":
-                return "case34";
-                break;
-
-        case "case35":
-                return "case35";
-                break;
-
-        case "case36":
-                return "case36";
-                break;
-
-        case "case37":
-                return "case37";
-                break;
-
-        case "case38":
-                return "case38";
-                break;
-
-        case "case39":
-                return "case39";
-                break;
-
-        case "case39":
-                return "case39";
-                break;
-	}
+let tabCases = {
+        "case0" : new THREE.Vector3(0.335*11.5, 2, 0.335*11.5), 
+  
+        "case1"  : new THREE.Vector3(0.335*10, 2, 0.335*11.5),
+        "case2" : new THREE.Vector3(0.335*9, 2, 0.335*11.5), 
+        "case3"  : new THREE.Vector3(0.335*8, 2, 0.335*11.5), 
+        "case4"  : new THREE.Vector3(0.335*7, 2, 0.335*11.5), 
+        "case5"  : new THREE.Vector3(0.335*6, 2, 0.335*11.5),
+        "case6"  : new THREE.Vector3(0.335*5, 2, 0.335*11.5), 
+        "case7"  : new THREE.Vector3(0.335*4, 2, 0.335*11.5), 
+        "case8"  : new THREE.Vector3(0.335*3, 2, 0.335*11.5), 
+        "case9"  : new THREE.Vector3(0.335*2, 2, 0.335*11.5), 
+        
+        "case10"  : new THREE.Vector3(0.335*1,2,0.335*11.5),
+        
+        "case11"  : new THREE.Vector3(0.335*1,2,0.335*10), 
+        "case12"  : new THREE.Vector3(0.335*1,2,0.335*9),
+        "case13"  : new THREE.Vector3(0.335*1,2,0.335*8), 
+        "case14"  : new THREE.Vector3(0.335*1,2,0.335*7), 
+        "case15"  : new THREE.Vector3(0.335*1,2,0.335*6), 
+        "case16"  : new THREE.Vector3(0.335*1,2,0.335*5), 
+        "case17"  : new THREE.Vector3(0.335*1,2,0.335*4), 
+        "case18"  : new THREE.Vector3(0.335*1,2,0.335*3),
+        "case19"  : new THREE.Vector3(0.335*1,2,0.335*2), 
+        
+        "case20"  : new THREE.Vector3(0.335*1,2,0.335*1), 
+        
+        "case21"  : new THREE.Vector3(0.335*2,2,0.335*1), 
+        "case22"  : new THREE.Vector3(0.335*3,2,0.335*1), 
+        "case23"  : new THREE.Vector3(0.335*4,2,0.335*1), 
+        "case24"  : new THREE.Vector3(0.335*5,2,0.335*1), 
+        "case25"  : new THREE.Vector3(0.335*6,2,0.335*1), 
+        "case26"  : new THREE.Vector3(0.335*7,2,0.335*1), 
+        "case27"  : new THREE.Vector3(0.335*8,2,0.335*1), 
+        "case28"  : new THREE.Vector3(0.335*9,2,0.335*1), 
+        "case29"  : new THREE.Vector3(0.335*10,2,0.335*1), 
+        
+        "case30"  : new THREE.Vector3(0.335*11.5,2,0.335*1), 
+        "case31"  : new THREE.Vector3(0.335*11.5,2,0.335*2), 
+        "case32"  : new THREE.Vector3(0.335*11.5,2,0.335*3), 
+        "case33"  : new THREE.Vector3(0.335*11.5,2,0.335*4), 
+        "case34"  : new THREE.Vector3(0.335*11.5,2,0.335*5), 
+        "case35"  : new THREE.Vector3(0.335*11.5,2,0.335*6), 
+        "case36"  : new THREE.Vector3(0.335*11.5,2,0.335*7), 
+        "case37"  : new THREE.Vector3(0.335*11.5,2,0.335*8), 
+        "case38"  : new THREE.Vector3(0.335*11.5,2,0.335*9), 
+        "case39"  : new THREE.Vector3(0.335*11.5,2,0.335*10) 
 }
